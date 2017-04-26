dbdbfa28238dd0f1e35866c69feea79a0ae7253a
kmorrow931 <kmorrow302@gmail.com>
2017-04-25 23:41:09 -0700

    Add files via upload
    Final Project Submission...hopefully

---
+++ kmorrow931.github.io/assets/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	// $(function(){
+	// 	$(.typed-message).typed ({
+	// 		strings:["Welcome to Miss Morrow's", "Fourth Grade Class Website!"],
+	// 		typeSpeed:4s;
+	// 	});
+		// });
+	$("td").hover({
+		function(){
+			$(this).addClass("emphasis");
+	}
+
+	$(".button-1",".button-2",".button-3").click(function(){
+			$("image-holder").effect("bounce",700, 300);
+    		$("h1", ".image-holder",".button-1",".button-2",".button-3","#calendar-header","table","#computer-holder").fadeIn(3s);
+});
+		}
+	});
+	$()	
+
+
+});	
