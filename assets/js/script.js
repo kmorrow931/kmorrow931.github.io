df7cee4c71d962327a4d12bc01f0037cd0bba227
kmorrow931 <kmorrow302@berkeley.edu>
2017-04-26 16:42:27 -0700

    More changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,33 @@
 $(document).ready(function() {
-	// $(function(){
+    		$(".instruction-1").fadeIn(20s)
+    		$(".instruction-1").fadeOut([20s][40s]);
+    		$(".instruction-2").fadeIn(30s);
+    	});
+    			// $(function(){
 	// 	$(.typed-message).typed ({
 	// 		strings:["Welcome to Miss Morrow's", "Fourth Grade Class Website!"],
 	// 		typeSpeed:4s;
 	// 	});
 		// });
-	$("td").hover({
+	$("td").hover(
 		function(){
-			$(this).addClass("emphasis");
-	}
-
-	$(".button-1",".button-2",".button-3").click(function(){
-			$("image-holder").effect("bounce",700, 300);
-    		$("h1", ".image-holder",".button-1",".button-2",".button-3","#calendar-header","table","#computer-holder").fadeIn(3s);
-});
-		}
-	});
-	$()	
+			if (!$(this).hasClass("month")) {
+				$(this).addClass("emphasis");
+			}
+		}, function() {
+			$(this).removeClass("emphasis");
+		}	
+	);
 
 
-});	
+	// $(".button").click(function(){
+	// 		$(".image-holder").toggle( "bounce", { times: 3 }, "slow" );
+		
+	// });
+	// 	}
+	// });
+
+	// $()	
+
+
+};	
